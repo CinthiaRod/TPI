4738835f4da0de2d2a9a7b14fb5cbc2eabef995f
paolajdelgado <delgadog.paola@gmail.com>
2025-06-26 16:41:16 -0700

    Modificacion del idioma de los mensajes(ingles) y comentarios en archivos Controller-Middlewares-Index

--- BACKEND/MIDDLEWARES/ErrorMiddleware.js
+++ BACKEND/MIDDLEWARES/ErrorMiddleware.js
@@ -1,6 +1,8 @@
+//Funcion para manejar errores
 const errorMiddleware = (err, req, res, next) => {
     console.error('Error: ', err.message); // Muestra el mensaje de error en la consola del servidor
     res.status(500).json({ error: "Ocurrio un error en el servidor" }); // Envía una respuesta de error al cliente
 };
 
+//Exportar modulo
 module.exports = errorMiddleware;
--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,24 +1,32 @@
+//Importar modulos
 const express = require('express');
 const dotenv = require('dotenv');
 const bookRoutes = require('./ROUTES/bookRoutes');
 const usersRoutes = require ('./ROUTES/usersRoutes')
 const errorMiddleware = require('./MIDDLEWARES/ErrorMiddleware');
 
-dotenv.config(); // Cargar variables de entorno desde .env 
+// Cargar variables de entorno desde .env 
+dotenv.config(); 
 
+//Crear app con express
 const app = express();
-const PORT = process.env.PORT || 3000; // Puerto configurable 
+//Puerto configurable, segun lo indicado en archivo ".env"
+const PORT = process.env.PORT || 3000; // En caso de que falte la información, por defecto sera "3000"
 
-app.use(express.json()); // Middleware para parsear archivo JSON
+// Middleware para parsear archivo JSON
+//Es decir, interpretar automáticamente el cuerpo de las solicitudes entrantes en formato JSON.
+app.use(express.json()); 
+//Middleware para obtener la información del front
 app.use(express.static('PUBLIC'));
 
 // Rutas de la API
-app.use('/libros', bookRoutes); 
-app.use('/users', usersRoutes); 
+app.use('/books', bookRoutes); //para las consultas de los libros
+app.use('/users', usersRoutes); //para las consultas de los usuarios
 
 // Middleware de manejo de errores global
 app.use(errorMiddleware);
 
+//Iniciar el servidor y hacer que escuche en un puerto especifico
 app.listen(PORT, () => {
-     console.log(`Servidor escuchando en http://localhost:${PORT}`);
+     console.log(`Server listen at http://localhost:${PORT}`);
 });
